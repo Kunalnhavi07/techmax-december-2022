502894e2612502cd2845b7f7080cc117cab323cb
House Of Computer <House Of Computer@DESKTOP-QV4B830>
2023-03-11 13:04:52 +0530

    first commit......

---
+++ eclipse-workspace-java projects/git and github/src/gitandgithub/Test2.java
@@ -0,0 +1,19 @@
+package gitandgithub;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String a="hello";
+		
+		if(a=="hello")
+		{
+			System.out.println("welcome");
+		}
+		
+		else
+		{
+			System.out.println("hii");
+		}
+	}
+
+}
