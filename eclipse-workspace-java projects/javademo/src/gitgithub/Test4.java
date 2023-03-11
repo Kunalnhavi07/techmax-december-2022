026ca84605d1c9f4880af1149be309f2abdc9bc1
House Of Computer <House Of Computer@DESKTOP-QV4B830>
2023-03-11 21:01:51 +0530

    First commit

---
+++ eclipse-workspace-java projects/javademo/src/gitgithub/Test4.java
@@ -0,0 +1,18 @@
+package gitgithub;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		int n=50;
+		
+		for(i=1;i<=n;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
