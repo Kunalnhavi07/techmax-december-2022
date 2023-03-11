3decae2a75ee122650aaf9676ec359a6f8732e60
HP <HP@DESKTOP-5T8Q470>
2023-03-11 21:53:09 +0530

    First Commit

---
+++ javademo1/src/gitandgithub/GitAndGitHub.java
@@ -0,0 +1,14 @@
+package gitandgithub;
+
+public class GitAndGitHub {
+
+	public static void main(String[] args) {
+		int i=1;
+		int n=50;
+		
+		for(i=1; i<n; i++)
+		System.out.println(i);
+
+	}
+
+}
